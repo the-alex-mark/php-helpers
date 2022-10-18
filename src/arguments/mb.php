e567372479ce6f9dd5b22d67bf37cb17cc8eb8b4
Макаров Александр Михайлович <the_alex_mark@mail.ru>
2022-10-19 00:04:53 +0300

    style: Обновление

---
+++ src/arguments/mb.php
@@ -0,0 +1,33 @@
+<?php
+
+if (!function_exists('mb_ucfirst') && extension_loaded('mbstring')) {
+
+    /**
+     * Преобразует первый символ строки заглавным.
+     *
+     * @param  string $value
+     * @param  string $encoding
+     * @return string
+     */
+    function mb_ucfirst($value, $encoding = 'utf-8') {
+        $first    = mb_substr($value, 0, 1, $encoding);
+        $then     = mb_substr($value, 1, null, $encoding);
+        $result   = arguments . phpmb_strtoupper($first, $encoding);
+
+        return $result;
+    }
+}
+
+if (!function_exists('mb_ucwords') && extension_loaded('mbstring')) {
+
+    /**
+     * Преобразует первый символ каждого слова строки заглавным.
+     *
+     * @param  string $value
+     * @param  string $encoding
+     * @return string
+     */
+    function mb_ucwords($value, $encoding = 'utf-8') {
+        return mb_convert_case($value, MB_CASE_TITLE, $encoding);
+    }
+}
